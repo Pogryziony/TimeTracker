bc597f0c9f26a9f8341c8e4e9ee00cbbc7cc3369
Pogryziony <Pogryziony98@gmail.com>
2020-07-16 19:19:06 +0200

    New repository: TimeDataRepository

--- src/main/java/com/github/Pogryziony/TimeTracker/Data/TimeDataRepository.java
+++ src/main/java/com/github/Pogryziony/TimeTracker/Data/TimeDataRepository.java
@@ -1,5 +1,14 @@
 package com.github.Pogryziony.TimeTracker.Data;
 
-public class TimeDataRepository {
+
+//Dla naszej encji definiujemy repozytorium JPA. Ponieważ będziemy wykonywać tylko podstawowe operacje, to nie jest konieczne definiowanie żadnych własnych metod
+
+
+import com.github.Pogryziony.TimeTracker.Model.TimeData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TimeDataRepository extends JpaRepository<TimeData,Long> {
 
 }
